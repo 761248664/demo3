c855a98fda092aa6a37db3d813eafaf0f139be80
SAMSUNG <761248664@qq.com>
2019-11-02 20:29:26 +0800

    本地工程添加到Git

--- GitTest2/src/main/java/Demo1.java
+++ GitTest2/src/main/java/Demo1.java
@@ -5,4 +5,8 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("11123311");
     }
+    public void test1(){
+        int i=0;
+        i +=10;
+    }
 }
